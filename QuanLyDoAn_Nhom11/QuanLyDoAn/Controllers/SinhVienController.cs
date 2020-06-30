6dcd1d38692aa06aba71a8a7fdac13ff7617487b
hoanganhtrans2 <46130871+hoanganhtrans2@users.noreply.github.com>
2020-06-30 15:33:49 +0700

    Truy vấn

---
+++ QuanLyDoAn_Nhom11/QuanLyDoAn/Controllers/SinhVienController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyDoAn.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        // GET: SinhVien
+        public ActionResult DanhSachDeTai()
+        {
+            QLDoAnSinhVienEntities db = new QLDoAnSinhVienEntities();
+            var detais = db.DeTais.ToList();
+            return View(detais);
+        }
+    }
+}
---
+++ QuanLyDoAn_Nhom11/QuanLyDoAn/Models/DeTaiModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyDoAn.Models
+{
+    public class DeTaiModel
+    {
+        public string MaDeTai { get; set; }
+
+        public string TenDeTai { get; set; }
+
+        public int SoSinhVienThamGia { get; set; }
+
+        public string GhiChu { get; set; }
+
+        public int SoSinhVienDangKy { get; set; }
+    }
+}
